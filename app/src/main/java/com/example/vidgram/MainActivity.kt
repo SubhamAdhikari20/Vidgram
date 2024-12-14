c4a16d5fbe596cc08eca4b7aef09a66428c3855b
Yogesh25900 <chaudharyyogesh258@gmail.com>
2024-12-14 13:02:35 +0545

    Intialize firebase sdk

--- app/src/main/java/com/example/vidgram/MainActivity.kt
+++ app/src/main/java/com/example/vidgram/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.vidgram
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)  // starting the intent of login activty :) happy coding
+            finish() // to close the splash screen
+        }, 2000) // 1000ms(millisecond) = 1 second  calculate bro math...shit here we go again maths
     }
+
+
+    //could have an animated logo for the app...
+
 }
