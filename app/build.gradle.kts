4d598c40609d0a420e8a4006d81c268422d6737e
Subham Adhikari <subhamadhikari20@gmail.com>
2024-12-19 15:30:02 +0545

    Updated Authentication Pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,16 @@ plugins {
 }
 
 android {
+    buildFeatures{
+        viewBinding = true
+    }
     namespace = "com.example.vidgram"
     compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.vidgram"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -34,10 +37,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
-    buildFeatures{
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -47,8 +46,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.database)
-    implementation(libs.firebase.auth)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/vidgram/MainActivity.kt
+++ app/src/main/java/com/example/vidgram/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vidgram
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -8,23 +9,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vidgram.databinding.ActivitySplashScreenBinding
+
 
 // Splash Screen - Vidgram
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)   // starting the intent of login activty :) happy coding
+            finish()                // to close the splash screen
+        }, 2000)          // 1000ms(millisecond) = 1 second  calculate bro math...shit here we go again maths
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intent)  // starting the intent of login activty :) happy coding
-            finish() // to close the splash screen
-        }, 2000) // 1000ms(millisecond) = 1 second  calculate bro math...shit here we go again maths
+
+
     }
 
 
