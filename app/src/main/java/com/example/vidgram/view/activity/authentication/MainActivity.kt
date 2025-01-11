72a23a28f0879ab0ea28264dafb02bc61ab4fcc1
Subham Adhikari <subhamadhikari20@gmail.com>
2025-01-11 11:28:02 +0545

    Completion of Sprint 4 - Implementation of Backend/Firebase Database with MVVM with Repository pattern.

--- app/src/main/java/com/example/vidgram/view/activity/authentication/MainActivity.kt
+++ app/src/main/java/com/example/vidgram/view/activity/authentication/MainActivity.kt
@@ -9,13 +9,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.vidgram.R
+import com.example.vidgram.databinding.ActivityMainBinding
 
 // Splash Screen - Vidgram
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_splash_screen)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
