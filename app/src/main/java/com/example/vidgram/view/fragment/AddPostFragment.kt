0a422fa64a6543d442eefb14d539818b3bf6bfb3
Subham Adhikari <subhamadhikari20@gmail.com>
2025-01-16 21:04:22 +0545

    Adding Add Post Feature

--- app/src/main/java/com/example/vidgram/view/fragment/AddPostFragment.kt
+++ app/src/main/java/com/example/vidgram/view/fragment/AddPostFragment.kt
@@ -9,10 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.example.vidgram.R
 import com.example.vidgram.databinding.FragmentAddPostBinding
-import com.example.vidgram.repository.PostRepositoryImpl
-import com.example.vidgram.utils.LoadingDialogUtils
 import com.example.vidgram.view.activity.NewPostActivity
-import com.example.vidgram.viewmodel.PostViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddPostFragment : BottomSheetDialogFragment() {
@@ -35,8 +32,6 @@ class AddPostFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
         // Set up click listeners for the options
         binding.takePhotoLinearLayout.setOnClickListener {
             val intent = Intent(requireContext(), NewPostActivity::class.java)
@@ -47,9 +42,6 @@ class AddPostFragment : BottomSheetDialogFragment() {
             val intent = Intent(requireContext(), NewPostActivity::class.java)
             startActivity(intent)
         }
-
-
-
 
     }
 
