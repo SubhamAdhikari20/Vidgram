fad934b435ed1815bb06f1ec2bfec179983b531e
Yogesh25900 <chaudharyyogesh258@gmail.com>
2025-01-17 21:20:32 +0545

    working on post feature -2

--- app/src/main/java/com/example/vidgram/model/Post.kt
+++ app/src/main/java/com/example/vidgram/model/Post.kt
@@ -3,28 +3,17 @@ package com.example.vidgram.model
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-//@Parcelize
-//data class Post(
-//    val username: String,        // Username of the post author
-//    val userAvatar: String,         // Resource ID for the user's avatar image
-//    val postImage: String,          // Resource ID for the post image
-//    val caption: String,          // Caption of the post
-//    val time: String,          // Time of the post
-//    val like: String,          // Like of the post
-//    val dislike: String,         // DisLike of the post
-//    val comment: String,         // Comment of the post
-//    val share: String,         // Share of the post
-//) : Parcelable
-
+@Parcelize
 data class Post(
     val username: String,        // Username of the post author
-    val userAvatar: Int,         // Resource ID for the user's avatar image
-    val postImage: Int,          // Resource ID for the post image
+    val userAvatar: String,         // Resource ID for the user's avatar image
+    val postImage: String,          // Resource ID for the post image
     val caption: String,          // Caption of the post
     val time: String,          // Time of the post
     val like: String,          // Like of the post
     val dislike: String,         // DisLike of the post
     val comment: String,         // Comment of the post
     val share: String,         // Share of the post
-)
+) : Parcelable
--- app/src/main/java/com/example/vidgram/view/activity/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/vidgram/view/activity/BottomNavigationActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vidgram.view.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.window.OnBackInvokedDispatcher
 import androidx.activity.enableEdgeToEdge
@@ -10,22 +11,24 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Observer
 import com.example.vidgram.R
 import com.example.vidgram.databinding.ActivityBottomNavigationBinding
+import com.example.vidgram.model.Post
 import com.example.vidgram.view.fragment.CommunityFragment
 import com.example.vidgram.view.fragment.HomeFragment
 import com.example.vidgram.view.fragment.MessageFragment
 import com.example.vidgram.view.fragment.NotificationFragment
 import com.example.vidgram.view.fragment.AddPostFragment
-import com.example.vidgram.view.fragment.NewAddPost
-import com.example.vidgram.viewmodel.SharedViewModel
+import com.example.vidgram.viewmodel.PostViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class BottomNavigationActivity : AppCompatActivity() {
-    private val sharedViewModel: SharedViewModel by viewModels() // Shared ViewModel
+    private lateinit var postViewModel: PostViewModel
 
     private lateinit var binding : ActivityBottomNavigationBinding
     lateinit var bottomSheetDialog: BottomSheetDialog
+    private val ADD_POST_REQUEST_CODE = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +51,13 @@ class BottomNavigationActivity : AppCompatActivity() {
             true
         }
 
+
+        // In BottomNavigationActivity
+        postViewModel.posts.observe(this, Observer { updatedPosts ->
+            val homeFragment = supportFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName) as? HomeFragment
+            homeFragment?.postAdapter?.updatePosts(updatedPosts)  // Update HomeFragment's adapter
+        })
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomNavigationLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
@@ -56,8 +66,10 @@ class BottomNavigationActivity : AppCompatActivity() {
     }
 
     private fun showAddPostDialog() {
-        val addPostFragment =  NewAddPost()
-        addPostFragment.show(supportFragmentManager, "AddPostFragment")
+        val intent = Intent(this, NewPostActivity::class.java)
+        startActivityForResult(intent, ADD_POST_REQUEST_CODE)
+//        val addPostFragment =  AddPostFragment()
+//        addPostFragment.show(supportFragmentManager, "AddPostFragment")
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -73,8 +85,18 @@ class BottomNavigationActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frameLayout, fragment)
         fragmentTransaction.commit()
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == ADD_POST_REQUEST_CODE && resultCode == RESULT_OK) {
+            val newPost = data?.getParcelableExtra<Post>("new_post")
+            newPost?.let {
+                // Pass post to HomeFragment's PostAdapter
+                val homeFragment =
+                    supportFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName) as? HomeFragment
+                homeFragment?.addPostToAdapter(it)
+            }
+        }
 
 
-
-
-}
+    }}
